6c31eccd1414d779bb395b43b717ed3d40a1472b
Sergio Antozzi <santozzi@gmail.com>
2022-05-31 15:28:19 -0300

    actualizacion del redme, estilo de imágen, icono y titulo de la página

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Próximamente
+        </h1>
+
       </header>
     </div>
   );
