3fcc90f53fa02f7e675c2b37748dcd209d01e783
Sergio Antozzi <santozzi@gmail.com>
2022-06-25 14:51:21 -0300

    agrego NavLink a CartWidget

--- src/components/navBar/cartWidjet/cartWidjet.js
+++ src/components/navBar/cartWidjet/cartWidjet.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './cartWidjet.css';
 export const CartWidjet = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
+    //onClick={() => navigate('/cart')}
+    const active = {
+        color: 'red',
+    }
+
+    const disactive = {
+        default: {
+            color: "#eee"
+        },
+        Hovered: {
+            color: 'red'
+        }
+
+
+    }
     return (
-        <button className='cart-widjet-btn' onClick={() => navigate('/cart')}><FaShoppingCart /></button>
+        <NavLink to='/cart' className='cart-widjet-btn' style={({ isActive }) => (isActive ? active : disactive)}><FaShoppingCart /></NavLink>
     );
 }
