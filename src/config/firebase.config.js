b8bfe275fd5b91c7a89f2bfae4342d47b033563e
Sergio Antozzi <santozzi@gmail.com>
2022-07-01 21:08:28 -0300

    cambio de configuracion .env

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,10 +1,23 @@
+//luego estas constantes se van a .env
+
+
+const REACT_APP_API_KEY = "AIzaSyAWxct56Lnj4w56B38rux153cvCxpa40f4"
+const REACT_APP_AUTH_DOMAIN = "universo-3d.firebaseapp.com"
+const REACT_APP_PROJECT_ID = "universo-3d"
+const REACT_APP_STORAGE_BUCKET = "universo-3d.appspot.com"
+const REACT_APP_MESSAGING_SENDER_ID = "956668652"
+const REACT_APP_APP_ID = "1:956668652:web:c78fbd05b08eed9c1aa3c9"
+
+
+
+
 export const firebaseUniverso3d = {
     nameDB: "universo-3d",
-    API_KEY: process.env.REACT_APP_API_KEY,
-    AUTH_DOMAIN: process.env.REACT_APP_AUTH_DOMAIN,
-    PROJECT_ID: process.env.REACT_APP_PROJECT_ID,
-    STORAGE_BUCKET: process.env.REACT_APP_STORAGE_BUCKET,
-    MESSAGING_SENDER_ID: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    APP_ID: process.env.REACT_APP_APP_ID
+    API_KEY: REACT_APP_API_KEY,
+    AUTH_DOMAIN: REACT_APP_AUTH_DOMAIN,
+    PROJECT_ID: REACT_APP_PROJECT_ID,
+    STORAGE_BUCKET: REACT_APP_STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: REACT_APP_MESSAGING_SENDER_ID,
+    APP_ID: REACT_APP_APP_ID
 
 }
